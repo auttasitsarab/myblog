c25e1328bbf86116d51b5de28752cc2981a88574
auttasitsarab <auttasitsarab@hotmail.com>
2024-06-25 20:54:16 +0700

    ADD file

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 git remote add origin https://github.com/auttasitsarab/myblog.git
-git branch -M main
-git commit -a "ADD file"
+git add -A
+git commit -m "ADD file"
 git push -u origin main
 sleep 50
