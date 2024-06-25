2c0467318abad7de25cf19f006eb380245d0d87d
auttasitsarab <auttasitsarab@hotmail.com>
2024-06-25 21:29:42 +0700

    ADD file

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
+git config --global user.auttasitsarab
+git config --global user.auttasitsarab@hotmail.com
 git remote add origin https://github.com/auttasitsarab/myblog.git
 git add -A
 git commit -m "ADD file"
 git push
+chmod +x gitpush.sh
 sleep 30
